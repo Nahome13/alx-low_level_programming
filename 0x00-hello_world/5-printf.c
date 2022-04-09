53ed4edf2fc11884316805faa21ddbbb59525cfe
Nahome13 <nahomeshetu13@gmail.com>
2022-04-09 13:26:31 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
-#include <stdio.h>                                                              
+#include <stdio.h>
 
 /**
- * main -print out a string to 
+ * main -print out a string to
  *
  * Return: Always 0 (success)
  */
- 
 int main(void)
 {   
-printf("with proper grammar, but the outcome is a piece of art,\n");            return(0);                                                           
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return(0);
 }
