334837c9dc48d7c12db709695ab5047b06a89320
Nahome13 <nahomeshetu13@gmail.com>
2022-04-09 12:24:45 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *
+ *main - entry point
+ *
+ *Description: Print lowercase alphabet backwards
+ *
+ *Return: 0
+ */
+    
+int main(void)
+
+{
+
+char c;
+
+for (c = 'z'; c >= 'a'; --c)
+
+putchar(c);
+putchar('\n');
+
+return (0);
+
+}
