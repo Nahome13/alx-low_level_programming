17f37e657de622b3cadc4b037e0df5399302d3a7
Nahome13 <nahomeshetu13@gmail.com>
2022-04-09 02:11:10 -0700

    ad

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,7 +6,7 @@
  * main - ptint alphabet in low cases
  *
  * Describtion: to print alphabet in low casedd.
- * return: always (0)
+ * return: always 0
  *
  */
 int main(void)
@@ -16,5 +16,6 @@ int c;
 for (c = 0; c < 26; ++c)
 putchar('a' + c);
 putchar('\n');
-return (0);	
+
+return (0);
 }
