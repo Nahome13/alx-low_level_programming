e964622886ad2919c42acb17a8831b8e7bbb9206
Nahome13 <nahomeshetu13@gmail.com>
2022-04-12 11:38:42 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,7 +4,7 @@
 /**
  * times_table - Prints the 9 times table, starting with 0.
  *
- */   
+ */
 
 void times_table(void)
 
@@ -49,7 +49,5 @@ void times_table(void)
 		}
 
 		_putchar('\n');
-
 	}
-
 }
