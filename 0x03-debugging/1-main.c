8b3c606d01327d5cde6ee31526138209bd4f53ca
Nahome13 <nahomeshetu13@gmail.com>
2022-04-13 09:43:44 -0700

    1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,7 +5,6 @@
  * Return: 0
  *
  */    
-
 int main(void)
 {
 
@@ -17,17 +16,14 @@ int main(void)
 
 	/*
 	 * while (i < 10)
-	 *
-	 * *{
+	 * {
 	 *
 	 * *Infinite loop - No increase of variable
 	 *
 	 * *putchar(i);
 	 *
 	 * }
-	 *
 	 */ 
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
