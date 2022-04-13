0dfc4b07ecc4fcb6ff88f141476939328d791f52
Nahome13 <nahomeshetu13@gmail.com>
2022-04-13 09:20:14 -0700

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * main - main
- *
+ * main - main int void
+ * Description: positive and negative
  * Return: 0
  */
    
 int main(void)
 {
-
 	int i;
-
 	i = 0;
 
 	positive_or_negative(i);
