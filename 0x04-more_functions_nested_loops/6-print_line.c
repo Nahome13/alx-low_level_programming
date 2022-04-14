adfeb2020aea01b9b2c093c5775048452a387f75
Nahome13 <nahomeshetu13@gmail.com>
2022-04-14 12:16:36 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,24 +1,30 @@
 #include "main.h"
 
-/*
- * print_line - prints underscores`
+
+/**
+ * print_line - prints underscores
  *
  * @n: limit
+ *
  * Return: void
  */
 void print_line(int n)
 {
+
 	int i;
 
 	if (n > 0)
 
 	{
+
 		for (i = 0; i < n; i++)
 
 		{
 
 			_putchar('_');
+
 		}
+
 	}
 
 	_putchar('\n');
