0f3435a09849b85c85d18357da59db7065f45296
Nahome13 <nahomeshetu13@gmail.com>
2022-04-18 09:18:08 -0700

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,13 +4,12 @@
  * @n: a pinter that will be updated
  * return- 0
  */
-int main(void)
-{
-	int n;
+void reset_to_98(int *n)
 
-	n=402;
-printf("n=%d\n", n);
-reset_to_98(&n);
-printf("n=%d\n", n);
+{
+
+		*n = 98;
+
+}
 return(0);
 }
