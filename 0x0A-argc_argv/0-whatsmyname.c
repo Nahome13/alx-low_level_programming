95729025b8d5ee3adf1698163ecbda05f9ea01d3
Nahome13 <nahomeshetu13@gmail.com>
2022-05-04 12:10:41 -0700

    2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 
 /*
- *  * main - entry point prints the name of the program
+ * main - entry point prints the name of the program
+ * @argc: size of argv
  *
- *   * @argc: size of argv
- *
- *    * @argv: array of pointers to command line args passed
- *
- *     *
- *
- *      * Return: 0
- *
- *       */
+ * @argv: array of pointers to command line args passed
+ * Return: 0
+ */
 
 int main(int argc __attribute__((unused)), char *argv[])
-
 {
 
 		printf("%s\n", argv[0]);
